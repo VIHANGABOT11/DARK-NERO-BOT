ee781b2d21f58d60fd5d21322d896a5944940766
VIHANGABOT11 <110660825+VIHANGABOT11@users.noreply.github.com>
2022-08-09 18:47:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['774579505'] //ur premium numbers
+global.premium = ['740505722'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
